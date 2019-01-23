dde592ad45ed981042de0c6bf984772fb194707e
Robert <robertbaddeley@gmail.com>
2019-01-22 14:43:54 -1000

    started connecting to front-end
    still need to hash, will wait for database integration.
    
    signin functionality working - with database integration will do for multiple users

--- server.js
+++ server.js
@@ -1,26 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
 const database = {
     users: [
         {
-            id: 123,
+            id: '123',
             name: 'John',
             email: 'john@gmail.com',
             password: 'cookies',
-            entries: 33,
+            entries: 0,
             joined: new Date(),
         },
         {
-            id: 234,
+            id: '234',
             name: 'Sally',
             email: 'sally@gmail.com',
-            password: 'bananas',
-            entires: 23,
+            password: 'apples',
+            entries: 0,
             joined: new Date(),
+        }
+    ],
+    login: [
+        {
+            id: '987',
+            hash: '',
+            email: 'john@gmail.com',
         }
     ]
 }
@@ -29,11 +40,11 @@ app.get('/', (req, res) => {
     res.send(database.users);
 })
 
-
+//signin route
 app.post('/signin', (req, res) => {
     if (req.body.email === database.users[0].email &&
         req.body.password === database.users[0].password) {
-            res.json(database.users[0]);
+            res.json('success');
     } else {
         res.status(400).json('error logging in');
     }
@@ -42,10 +53,14 @@ app.post('/signin', (req, res) => {
 
 })
 
+//register route
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
+    /* bcrypt.hash(password, null, null, function(err, hash) {
+        console.log(hash);
+    }); */
     database.users.push({
-        id: 142,
+        id: '142',
         name: name,
         email: email,
         password: password,
@@ -56,7 +71,42 @@ app.post('/register', (req, res) => {
     res.json(database.users[database.users.length-1]);
 })
 
+const findUser = id => {
+    const userArray = database.users.filter(user=>user.id===id);
+    return userArray[0]; //should never have more than 1 value returned, because it's a key
+}
 
+// profile route
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    const user = findUser(id);
+    if (user != undefined) { 
+        return res.json(user);
+    } else {
+        return res.status(400).json('not found');
+    }
+})
+
+// image route
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+    const user = findUser(id);
+    console.log('user', user);
+    if (user) {
+        user.entries++;
+        return res.json(user.entries);
+    } else {
+        return res.status(400).json('user not found');
+    }
+})
+
+/* bcrypt.hash("bacon", null, null, (err, hash) => {
+    //store hash in your password db
+})
+
+bcrypt.compare("bacon", hash, (err, res) => {
+    // res == true.
+}) */
 
 app.listen(3000, () => {
     console.log('server is running on port 3000');
